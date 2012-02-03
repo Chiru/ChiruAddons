80d79a6697f916e4763acad96649ac170ed69723
Miika Santala <miika.santala@cie.fi>
2012-02-03 13:16:37 +0200

    Add test scene for ObjectCapture.

---
+++ capture.js
@@ -0,0 +1,100 @@
+engine.ImportExtension("qt.core");
+engine.ImportExtension("qt.gui");
+
+var objectFinished = false;
+
+if(!server.IsRunning()) {
+    ObjectCapture();
+}
+
+function ObjectCapture()
+{
+    print("Starting object capture script");
+    var module = framework.GetModuleByName("ObjectCapture");
+    module.startCapturing();
+    
+    var screenEntity = scene.GetEntityByName("screen");
+    var component = screenEntity.GetComponent("EC_WidgetCanvas");
+    component.SetSubmesh(1);
+    component.SetRefreshRate(30);
+    
+    module.previewFrameUpdated.connect(updateScreen);
+    module.objectCaptured.connect(displayObject);
+    
+    var me = scene.GetEntityByName("ObjectCaptureApplication");
+    var inputmapper = me.GetOrCreateComponent("EC_InputMapper", 2, false);
+    
+    inputmapper.contextPriority = 100;
+    inputmapper.takeMouseEventsOverQt = false;
+    inputmapper.takeKeyboardEventsOverQt = false;
+    inputmapper.modifiersEnabled = false;
+    inputmapper.keyrepeatTrigger = false;
+    inputmapper.executionType = 1;
+
+    inputmapper.RegisterMapping("Y", "Capture", 1); // 1 = keypress
+    inputmapper.RegisterMapping("U", "Finalize", 1);
+    inputmapper.RegisterMapping("I", "startCapturing", 1);
+    inputmapper.RegisterMapping("O", "stopCapturing", 1);
+    inputmapper.RegisterMapping("P", "showObject", 1);
+    
+    me.Action("Capture").Triggered.connect(captureCloud);
+    me.Action("Finalize").Triggered.connect(finalizeCloud);
+    me.Action("startCapturing").Triggered.connect(startCapturing);
+    me.Action("stopCapturing").Triggered.connect(stopCapturing);
+    me.Action("showObject").Triggered.connect(showObject);
+}
+
+function updateScreen(image)
+{   
+    var screenEntity = scene.GetEntityByName("screen");
+    var component = screenEntity.GetComponent("EC_WidgetCanvas");
+    
+    component.Update(image);
+}
+
+function startCapturing()
+{
+    print("Starting capture interface.");
+    var module = framework.GetModuleByName("ObjectCapture");
+    module.startCapturing();
+}
+
+function stopCapturing()
+{
+    print("Stopping capture interface.");
+    var module = framework.GetModuleByName("ObjectCapture");
+    module.stopCapturing();
+}
+
+function captureCloud()
+{
+    print("Captured cloud.");
+    var module = framework.GetModuleByName("ObjectCapture");
+    module.captureCloud();
+}
+
+function finalizeCloud()
+{
+    print("Finalizing cloud.");
+    var module = framework.GetModuleByName("ObjectCapture");
+    module.finalizeCapturing();
+}
+
+function showObject()
+{
+    if(!objectFinished)
+        return;
+
+    print("Visualising final object.");
+    var module = framework.GetModuleByName("ObjectCapture");
+    
+    var entityId = module.capturedObject();
+    var entity = scene.GetEntity(entityId);
+    var placeable = entity.GetComponent("EC_Placeable");
+    placeable.visible = true;
+}
+
+function displayObject()
+{
+    objectFinished = true;
+}
