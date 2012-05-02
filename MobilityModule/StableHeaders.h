6ab510252d95a5966fa9d8ee9f8938df4ff76667
Jarkko Vatjus-Anttila <jvatjusanttila@gmail.com>
2012-05-02 14:13:27 +0300

    Quick fixes for MobilityModule to compile against latest tundra

--- MobilityModule/StableHeaders.h
+++ MobilityModule/StableHeaders.h
@@ -3,15 +3,16 @@
 #ifndef incl_MobileModule_StableHeaders_h
 #define incl_MobileModule_StableHeaders_h
 
-#include "CoreStdIncludes.h"
+//#include "CoreStdIncludes.h"
 
 // If PCH is disabled, leave the contents of this whole file empty to avoid any compilation unit getting any unnecessary headers.
 ///\todo Refactor the above #include inside this #ifdef as well.
 #ifdef PCH_ENABLED
 
-#include "Core.h"
-#include "Foundation.h"
+//#include "Core.h"
+//#include "Foundation.h"
 #include "Framework.h"
+#include "CoreDefines.h"
 
 #include <QtCore>
 #include <QtGui>
