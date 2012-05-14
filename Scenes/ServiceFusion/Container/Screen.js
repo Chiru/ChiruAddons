51aa0aa672fd164a51f958dc16e666ac250e6733
Stinkfist <stinkfist0@gmail.com>
2012-05-14 18:15:49 +0300

    Add null check for EC_GraphicsViewCanvas.

--- Scenes/ServiceFusion/Container/Screen.js
+++ Scenes/ServiceFusion/Container/Screen.js
@@ -1,13 +1,16 @@
 // Screen.js - Creates unique texture name for the GraphicsViewCanvas' output texture
 
-var content = me.GetComponent("EC_GraphicsViewCanvas", "Content");
-var title = me.GetComponent("EC_GraphicsViewCanvas", "Title");
-if (content && title)
+if (me.graphicsviewcanvas)
 {
-    content.outputTexture = asset.GenerateUniqueAssetName("Texture", "ScreenContent");
-    title.outputTexture =asset.GenerateUniqueAssetName("Texture", "ScreenTitle");
-}
-else
-{
-    me.graphicsviewcanvas.outputTexture = asset.GenerateUniqueAssetName("Texture", "Screen");
+    var content = me.GetComponent("EC_GraphicsViewCanvas", "Content");
+    var title = me.GetComponent("EC_GraphicsViewCanvas", "Title");
+    if (content && title)
+    {
+        content.outputTexture = asset.GenerateUniqueAssetName("Texture", "ScreenContent");
+        title.outputTexture =asset.GenerateUniqueAssetName("Texture", "ScreenTitle");
+    }
+    else
+    {
+        me.graphicsviewcanvas.outputTexture = asset.GenerateUniqueAssetName("Texture", "Screen");
+    }
 }
