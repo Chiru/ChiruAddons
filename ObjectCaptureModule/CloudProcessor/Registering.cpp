16df17f69951e0bbefd77da1d4c91c3df2e982cb
Miika Santala <miika.santala@cie.fi>
2012-05-21 14:59:57 +0300

    ObjectCaptureModule: Refactor to remove the RegisterInterface class. Add code for incremental register.

--- ObjectCaptureModule/CloudProcessor/Registering.cpp
+++ ObjectCaptureModule/CloudProcessor/Registering.cpp
@@ -1,52 +1,189 @@
 // For conditions of distribution and use, see copyright notice in LICENSE
 
 #include "Registering.h"
+#include "CloudFilter.h"
 
 #include <pcl/io/pcd_io.h>
+#include <pcl/registration/transforms.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/registration/icp.h>
 
 #include "LoggingFunctions.h"
 
 namespace ObjectCapture
 {
 
-RegisterInterface::RegisterInterface()
+IncrementalRegister::IncrementalRegister() :
+    cloud_filter_(new CloudFilter),
+    icp_corr_distance_(0.07f),
+    ransac_outlier_threshold_(0.05f),
+    uniform_leafsize_(0.01f)
 {
+
 }
 
-RegisterInterface::~RegisterInterface()
+void IncrementalRegister::registerCloud(PointCloud::ConstPtr cloud)
 {
-    clouds_.clear();
+    if(final_clouds_.size() == 0)
+    {
+        addCloudToFinalModel(cloud, Eigen::Matrix4f::Identity());
+    }
+    else
+    {
+        Eigen::Matrix4f transformation;
+
+        // Apply initial alignment (i.e. the alignment of the latest registered cloud).
+        PointCloud::Ptr initial_aligned(new PointCloud);
+        pcl::transformPointCloud(*cloud, *initial_aligned, getGlobalTransformation());
+
+        registerPair(getCurrentCloud(), initial_aligned, transformation);
+
+        addTransformation(transformation);
+        addCloudToFinalModel(initial_aligned, transformation);
+    }
+    emit globalModelUpdated(getCurrentCloud());
 }
 
-void RegisterInterface::addCloud(PointCloud::Ptr cloud)
+bool IncrementalRegister::removeLatestCloud()
 {
-    clouds_.append(cloud);
+    if(final_clouds_.size() <= 0)
+        return false;
+
+    removeLatestTransformation();
+    final_clouds_.erase(final_clouds_.end());
+    emit globalModelUpdated(getCurrentCloud());
+    return true;
 }
 
-void RegisterInterface::reset()
+void IncrementalRegister::reset()
 {
-    clouds_.clear();
+    transformations_.clear();
+    std::vector<PointCloud::Ptr>::iterator cloud_it;
+    for(cloud_it = final_clouds_.begin(); cloud_it != final_clouds_.end(); ++cloud_it)
+    {
+        (*cloud_it).reset();
+    }
+    final_clouds_.clear();
 }
 
-DummyRegister::DummyRegister() :
-    RegisterInterface()
+PointCloud::Ptr IncrementalRegister::getCurrentCloud()
 {
+    if(final_clouds_.size() <= 0)
+    {
+        return PointCloud::Ptr(new PointCloud);
+    }
+    else if(final_clouds_.size() == 1)
+    {
+        return final_clouds_.at(0);
+    }
+    else
+    {
+        PointCloud::Ptr full_cloud(new PointCloud);
+        std::vector<PointCloud::Ptr>::const_iterator it;
+        for(it = final_clouds_.begin(); it != final_clouds_.end(); ++it)
+        {
+            *full_cloud += *(*it);
+        }
+        return cloud_filter_->filterDensity(full_cloud, uniform_leafsize_);
+    }
 }
 
-DummyRegister::~DummyRegister()
+void IncrementalRegister::registerPair(PointCloud::Ptr src, PointCloud::Ptr tgt, Eigen::Matrix4f &pair_transformation)
 {
+    // Compute surface normals and curvature
+    PointCloudWithNormals::Ptr points_with_normals_src (new PointCloudWithNormals);
+    PointCloudWithNormals::Ptr points_with_normals_tgt (new PointCloudWithNormals);
+
+    pcl::NormalEstimation<PointT, PointNormalT> norm_est;
+    pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT> ());
+    norm_est.setSearchMethod (tree);
+    norm_est.setKSearch (30);
+
+    norm_est.setInputCloud (src);
+    norm_est.compute (*points_with_normals_src);
+    pcl::copyPointCloud (*src, *points_with_normals_src);
+
+    norm_est.setInputCloud (tgt);
+    norm_est.compute (*points_with_normals_tgt);
+    pcl::copyPointCloud (*tgt, *points_with_normals_tgt);
+
+    CurvaturePointRepresentation point_representation;
+    float alpha[4] = {1.0, 1.0, 1.0, 1.0};
+    point_representation.setRescaleValues (alpha);
+
+    pcl::IterativeClosestPoint<PointNormalT, PointNormalT> reg;
+    reg.setTransformationEpsilon (1e-6);
+    // Set the maximum distance between two correspondences (src<->tgt) to 10cm
+    // Note: adjust this based on the size of your datasets
+    reg.setMaxCorrespondenceDistance (0.07); // 0.3 w
+    reg.setRANSACOutlierRejectionThreshold(0.05); // 0.2 w
+    // Set the point representation
+    reg.setPointRepresentation (boost::make_shared<const CurvaturePointRepresentation> (point_representation));
+
+    reg.setInputCloud (points_with_normals_src);
+    reg.setInputTarget (points_with_normals_tgt);
+
+    Eigen::Matrix4f Ti = Eigen::Matrix4f::Identity (), prev, targetToSource;
+    PointCloudWithNormals::Ptr reg_result = points_with_normals_src;
+    reg.setMaximumIterations (2);
+    for (int i = 0; i < 60; ++i)
+    {
+      points_with_normals_src = reg_result;
+
+      // Estimate
+      reg.setInputCloud (points_with_normals_src);
+      reg.align (*reg_result);
+
+      // Accumulate transformation between each Iteration
+      Ti = reg.getFinalTransformation () * Ti;
+
+      // Refine the process
+      if (fabs ((reg.getLastIncrementalTransformation () - prev).sum ()) < reg.getTransformationEpsilon ()) {
+          //PCL_INFO("Decreasing maxium correspondence distance.\n");
+          reg.setMaxCorrespondenceDistance (reg.getMaxCorrespondenceDistance () - 0.005); // 0.001
+      }
+
+      prev = reg.getLastIncrementalTransformation ();
+
+      //PCL_INFO ("Iteration Nr. %d. (%f)\n", i, reg.getFitnessScore());
+
+      if(reg.getFitnessScore() < 0.0001f) {
+          //PCL_INFO("Convergence reached, breaking early.\n");
+          break;
+      }
+    }
+
+    pair_transformation = Ti.inverse();
 }
 
-PointCloud::Ptr DummyRegister::registerClouds()
+void IncrementalRegister::addCloudToFinalModel(PointCloud::ConstPtr cloud, Eigen::Matrix4f transformation)
 {
-    LogInfo("ObjectCapture: Registering clouds with method: DummyRegister..");
+    PointCloud::Ptr transformed_cloud(new PointCloud);
+    // Transform cloud to global coordinate system
+    pcl::transformPointCloud(*cloud, *transformed_cloud, transformation);
+    final_clouds_.push_back(transformed_cloud);
+}
 
-    PointCloud::Ptr output(new PointCloud);
+void IncrementalRegister::addTransformation(Eigen::Matrix4f &transformation)
+{
+    transformations_.push_back(transformation);
+}
 
-    if(clouds_.isEmpty())
-        return output;
+void IncrementalRegister::removeLatestTransformation()
+{
+    if(transformations_.size() > 0)
+        transformations_.erase(transformations_.end());
+}
 
-    return clouds_.at(0);
+Eigen::Matrix4f IncrementalRegister::getGlobalTransformation()
+{
+    Eigen::Matrix4f transformation = Eigen::Matrix4f::Identity();
+    std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> >::const_iterator it;
+    for(it = transformations_.begin(); it != transformations_.end(); ++it)
+    {
+        transformation = (*it) * transformation;
+    }
+    return transformation;
 }
 
 }
--- ObjectCaptureModule/CloudProcessor/Registering.h
+++ ObjectCaptureModule/CloudProcessor/Registering.h
@@ -4,41 +4,94 @@
 
 #include "ObjectCaptureModuleDefines.h"
 
+#include <pcl/point_representation.h>
+#include <Eigen/StdVector>
+#include <Eigen/Core>
+
 #include <QObject>
 #include <QList>
 #include <QString>
+#include <QMutex>
 
 namespace ObjectCapture
 {
+class CloudFilter;
 
-class RegisterInterface : public QObject
+/// Custom point presentation for points with pointnormal
+class CurvaturePointRepresentation : public pcl::PointRepresentation <PointNormalT>
+{
+    using pcl::PointRepresentation<PointNormalT>::nr_dimensions_;
+
+public:
+    CurvaturePointRepresentation ()
+    {
+        nr_dimensions_ = 4;
+    }
+
+    virtual void copyToFloatArray (const PointNormalT &p, float * out) const
+    {
+        out[0] = p.x;
+        out[1] = p.y;
+        out[2] = p.z;
+        out[3] = p.curvature;
+    }
+};
+
+// unused ?
+struct PCD
+{
+  PointCloud::Ptr cloud;
+  std::string f_name;
+
+  PCD() : cloud (new PointCloud) {}
+};
+
+// unused ?
+struct PCDComparator
+{
+  bool operator () (const PCD& p1, const PCD& p2)
+  {
+    return (p1.f_name < p2.f_name);
+  }
+};
+
+class IncrementalRegister : public QObject
 {
     Q_OBJECT
 
 public:
-    RegisterInterface();
-    virtual ~RegisterInterface();
+    IncrementalRegister();
+    ~IncrementalRegister();
 
-    void addCloud(PointCloud::Ptr cloud);
+    void registerCloud(PointCloud::ConstPtr cloud);
+
+    bool removeLatestCloud();
 
     void reset();
 
-    virtual PointCloud::Ptr registerClouds() = 0;
+    PointCloud::Ptr getCurrentCloud();
 
 protected:
-    QList<PointCloud::Ptr> clouds_;
+    void registerPair(PointCloud::Ptr src, PointCloud::Ptr tgt, Eigen::Matrix4f &pair_transformation);
+    void addCloudToFinalModel(PointCloud::ConstPtr cloud, Eigen::Matrix4f transformation);
+    void addTransformation(Eigen::Matrix4f &transformation);
+    void removeLatestTransformation();
+    Eigen::Matrix4f getGlobalTransformation();
 
-};
+private:
+    std::vector<PointCloud::Ptr> final_clouds_;
+    std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> > transformations_;
+    QMutex current_cloud_mutex_, registered_clouds_mutex_;
 
-class DummyRegister : public RegisterInterface
-{
-    Q_OBJECT
+    CloudFilter *cloud_filter_;
 
-public:
-    DummyRegister();
-    ~DummyRegister();
+    double icp_corr_distance_;
+    double ransac_outlier_threshold_;
+    double uniform_leafsize_;
 
-    virtual PointCloud::Ptr registerClouds();
+signals:
+    void globalModelUpdated(PointCloud::Ptr cloud);
+
 };
 
 }
