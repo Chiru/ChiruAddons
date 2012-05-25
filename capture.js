c1164c1f82ccaca8a14064493b3fc9cefc469ecc
Juha Hyvärinen <juha.hyvarinen@cie.fi>
2012-05-25 11:50:48 +0300

    ObjectCapture: Add simple GUI for capturing.

--- capture.js
+++ capture.js
@@ -9,7 +9,7 @@ if(!server.IsRunning()) {
 
 function ObjectCapture()
 {
-    print("Starting object capture script");
+    print("\nStarting object capture script");
     var module = framework.GetModuleByName("ObjectCapture");
     module.startCapturing();
     
@@ -42,6 +42,34 @@ function ObjectCapture()
     inputmapper.RegisterMapping("U", "Finalize", 1);
     inputmapper.RegisterMapping("I", "startCapturing", 1);
     inputmapper.RegisterMapping("O", "stopCapturing", 1);
+
+
+    var dialog = new QDialog();
+    dialog.setStyleSheet("QDialog {background-color: transparent;}");
+    var layout = new QHBoxLayout(dialog);
+
+    var buttonCapture = new QPushButton("Capture");
+    var buttonFinalize = new QPushButton("Finalize");
+    var buttonStart = new QPushButton("Start Capturing");
+    var buttonStop = new QPushButton("Stop Capturing");
+
+    layout.addWidget(buttonCapture, 0, Qt.AlignLeft);
+    layout.addWidget(buttonFinalize, 0, Qt.AlignLeft);
+    layout.addWidget(buttonStart, 0, Qt.AlignLeft);
+    layout.addWidget(buttonStop, 0, Qt.AlignLeft);
+
+    var proxy = new UiProxyWidget(dialog);
+
+    ui.AddProxyWidgetToScene(proxy);
+    proxy.x = 100;
+    proxy.y = 100;
+    proxy.windowFlags = 0;
+    proxy.visible = true;
+
+    buttonCapture.clicked.connect(captureCloud);
+    buttonFinalize.clicked.connect(finalizeCloud);
+    buttonStart.clicked.connect(startCapturing);
+    buttonStop.clicked.connect(stopCapturing);
     
     me.Action("Capture").Triggered.connect(captureCloud);
     me.Action("Finalize").Triggered.connect(finalizeCloud);
