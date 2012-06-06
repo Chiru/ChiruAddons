f4d490ef5c159f6e39d277b7f78d1b505c310fcd
Joosua <jonne.vaisanen@ludocraft.com>
2012-06-06 17:15:06 +0300

    Remove "FetchLocalNewsScript.h" include from testscript.cpp file.

--- 3dUiModule/TestScript.cpp
+++ 3dUiModule/TestScript.cpp
@@ -1,6 +1,5 @@
 // For conditions of distribution and use, see copyright notice in LICENSE
 
-#include "FetchLocalNewsScript.h"
 #include "LoggingFunctions.h"
 #include "IMemoryStore.h"
 #include "IWorld.h"
