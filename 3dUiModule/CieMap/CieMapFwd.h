05df9c60c45ea4d9614b8cf25c4540b91b0489cf
Stinkfist <stinkfist0@gmail.com>
2012-06-11 14:34:55 +0300

    Add missing CieMapFwd.h

---
+++ 3dUiModule/CieMap/CieMapFwd.h
@@ -0,0 +1,28 @@
+/**
+    For conditions of distribution and use, see copyright notice in LICENSE
+
+    @file   CieMapFwd.h
+    @brief  Forward declarations for commonly used CieMap types. */
+
+
+#pragma once
+
+// From RdfModule
+class IMemoryStore;
+
+namespace CieMap
+{
+    class IContainer;
+    class IEventManager;
+    class IVisualContainer;
+    class Layout;
+    class ContainerFactory;
+    class Container;
+    class ScriptManager;
+    class EventManager;
+    class HttpRequestResponse;
+    class IScript;
+    class IHttpRequestService;
+    class HttpRequest;
+    struct Tag;
+}
