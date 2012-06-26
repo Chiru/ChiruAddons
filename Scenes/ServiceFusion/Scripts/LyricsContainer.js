0f4862efa7b5c22278de73a734864f3d6cc4f6ad
Stinkfist <stinkfist0@gmail.com>
2012-06-26 16:55:55 +0300

    Add LookAtCamera functionality to LyricsContainer.

--- Scenes/ServiceFusion/Scripts/LyricsContainer.js
+++ Scenes/ServiceFusion/Scripts/LyricsContainer.js
@@ -5,6 +5,37 @@ engine.IncludeFile("RdfVocabulary.js");
 engine.IncludeFile("VisualContainerUtils.js");
 engine.IncludeFile("Lyrics.js");
 
+// LookAtCamera START
+const cAnimationTime = 1; // seconds
+var startOrientation, destOrientation;
+
+me.Action("MousePress").Triggered.connect(function()
+{
+    startOrientation = me.placeable.WorldOrientation();
+    var dir = me.placeable.WorldPosition().Sub(renderer.MainCamera().placeable.WorldPosition()).Normalized();
+    destOrientation = Quat.LookAt(scene.ForwardVector(), dir, scene.UpVector(), scene.UpVector());
+
+    timer = 0;
+    frame.Updated.connect(Animate);
+});
+
+var timer = 0;
+function Animate(dt)
+{
+    timer += dt;
+    if (timer < cAnimationTime)
+    {
+        var t = me.placeable.transform;
+        var q = Quat.Slerp(startOrientation, destOrientation, timer/cAnimationTime);
+        t.SetOrientation(q);
+        t.rot.y = 0;
+        me.placeable.transform = t;
+    }
+    else
+        frame.Updated.disconnect(Animate);
+}
+// LookAtCamera END
+
 var lyricslabel = null;
 
 me.placeable.visible = false;
