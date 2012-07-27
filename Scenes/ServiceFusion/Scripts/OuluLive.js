73425c4d12c4acc19da48b7b3555cf4de6d68281
Miika Santala <miika.santala@cie.fi>
2012-07-27 11:34:13 +0300

    ServiceFusion: Change Oulu Event Live to pull it's content from Oulu Events RSS feed

--- Scenes/ServiceFusion/Scripts/OuluLive.js
+++ Scenes/ServiceFusion/Scripts/OuluLive.js
@@ -34,7 +34,7 @@ function Animate(dt)
 }
 
 var webView = new QGraphicsWebView();
-webView.url = new QUrl("http://tapahtumakalenteri.ouka.fi");
+webView.url = new QUrl("http://chiru.cie.fi/servicefusion/OuluLive.html");
 webView.size = new QSize(me.graphicsviewcanvas.width, me.graphicsviewcanvas.height);
 me.graphicsviewcanvas.GraphicsScene().addItem(webView);
 
